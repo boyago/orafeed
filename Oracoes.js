85017c8df308b1b5505870604d70940fd630f17f
boyago <97127453+boyago@users.noreply.github.com>
2023-05-26 08:26:31 -0300

    Update Oracoes.js

--- Oracoes.js
+++ Oracoes.js
@@ -1,28 +1,23 @@
 [
-{
-    id: 1,
-    title: 'Ave Maria',
-    content: 'Ave, Maria, cheia de graça, o Senhor é convosco. Bendita sois vós entre as mulheres, e bendito é o fruto do vosso ventre, Jesus. Santa Maria, Mãe de Deus, rogai por nós pecadores, agora e na hora da nossa morte. Amém.',
-    image:'https://alexandreboyago.com.br/img/maria.jpg',
-    classificacao:'Tradicional'
-  },
-
   {
-    id: 2,
-    title: 'Pai Nosso',
-    content: 'Pai nosso, que estais nos céus, santificado seja o Vosso Nome, venha a nós o Vosso Reino, seja feita a Vossa vontade, assim na terra como no céu. O pão nosso de cada dia nos dai hoje, perdoai-nos as nossas ofensas, assim como nós perdoamos a quem nos tem ofendido, e não nos deixeis cair em tentação, mas livrai-nos do Mal. Amém.',
-    image:('https://bibliotecadopregador.com.br/wp-content/uploads/2022/08/Quem-e-Jesus-Cristo-na-Biblia.jpg'),
-    classificacao:'Tradicional'
+    "id": 1,
+    "title": "Ave Maria",
+    "content": "Ave, Maria, cheia de graça, o Senhor é convosco. Bendita sois vós entre as mulheres, e bendito é o fruto do vosso ventre, Jesus. Santa Maria, Mãe de Deus, rogai por nós pecadores, agora e na hora da nossa morte. Amém.",
+    "image": "https://alexandreboyago.com.br/img/maria.jpg",
+    "classificacao": "Tradicional"
   },
-
   {
-    id:3,
-    title:'Oração de Santa Rita de Cássia',
-    content:'Ó Poderosa e gloriosa Santa Rita, eis a vossos pés uma alma desamparada que necessitando de auxílio, a vós recorre com a doce esperança de ser atendida por vós que tem o título de Santa dos Casos Impossíveis e Desesperados.\n Ó cara santa interessai-vos pela minha causa, intercedei junto a Deus para que me conceda a graça que tanto necessito (faça o pedido).\n Não permitais que tenha de me afastar de vossos pés sem ser atendido. \n Se houver em mim algum obstáculo que me impeça de alcançar a graça que imploro, auxiliai-me para que o afaste. \n Envolvei o meu pedido em vossos preciosos méritos e apresentai-o a vosso celeste esposo, Jesus, em união com a vossa prece. Ó Santa Rita, eu ponho em vós toda a minha confiança. Por vosso intermédio, espero tranquilamente a graça que vos peço.' , 
-    image:('https://cdn.arquidiocesesorocaba.org.br/wp-content/uploads/2020/05/21154043/formacao_1600x1200-diante-dos-impossiveis-pecamos-socorro-a-santa-rita-de-cassia-1-1024x768.jpg'),
-    classificacao:'Santos'
+    "id": 2,
+    "title": "Pai Nosso",
+    "content": "Pai nosso, que estais nos céus, santificado seja o Vosso Nome, venha a nós o Vosso Reino, seja feita a Vossa vontade, assim na terra como no céu. O pão nosso de cada dia nos dai hoje, perdoai-nos as nossas ofensas, assim como nós perdoamos a quem nos tem ofendido, e não nos deixeis cair em tentação, mas livrai-nos do Mal. Amém.",
+    "image": "https://bibliotecadopregador.com.br/wp-content/uploads/2022/08/Quem-e-Jesus-Cristo-na-Biblia.jpg",
+    "classificacao": "Tradicional"
   },
-
-];
-
-
+  {
+    "id": 3,
+    "title": "Oração de Santa Rita de Cássia",
+    "content": "Ó Poderosa e gloriosa Santa Rita, eis a vossos pés uma alma desamparada que necessitando de auxílio, a vós recorre com a doce esperança de ser atendida por vós que tem o título de Santa dos Casos Impossíveis e Desesperados. Ó cara santa interessai-vos pela minha causa, intercedei junto a Deus para que me conceda a graça que tanto necessito (faça o pedido). Não permitais que tenha de me afastar de vossos pés sem ser atendido. Se houver em mim algum obstáculo que me impeça de alcançar a graça que imploro, auxiliai-me para que o afaste. Envolvei o meu pedido em vossos preciosos méritos e apresentai-o a vosso celeste esposo, Jesus, em união com a vossa prece. Ó Santa Rita, eu ponho em vós toda a minha confiança. Por vosso intermédio, espero tranquilamente a graça que vos peço.",
+    "image": "https://cdn.arquidiocesesorocaba.org.br/wp-content/uploads/2020/05/21154043/formacao_1600x1200-diante-dos-impossiveis-pecamos-socorro-a-santa-rita-de-cassia-1-1024x768.jpg",
+    "classificacao": "Santos"
+  }
+]
